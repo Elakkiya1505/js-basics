bc6b96b9eb97f4157f8289afc5e2c03e4bfea378
Elakkiya1505 <elakkiyar.23eie@kongu.edu>
2025-06-19 11:22:52 +0530

    complete conditionals topic

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+let marks =85;
+if(marks >=90){
+    console.log("A grage");
+} else if(marks >= 75){
+    console.log("B grade");
+} else {
+    console.log("C grade");
+}
+if (0) {
+    console.log("Working");
+} else  {
+    console.log("Falsy Value");
+}
